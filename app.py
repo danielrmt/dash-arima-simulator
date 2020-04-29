5be431606538c0e5df9d8adb5b0c7361eab7ceed
Daniel Ribeiro <danielrmt@gmail.com>
2020-04-29 09:08:26 -0300

    Generate param sliders based on p,q hyperparams

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import statsmodels.api as sm
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, ALL
 
 
 # ----
@@ -51,27 +51,21 @@ sidebar = [
     html.H6('Parâmetros', className='sidebar-heading text-muted'),
     html.Label('Tamanho da série', htmlFor='nsample'),
     dcc.Slider(id='nsample', min=30, max=1000, step=10, value=100,
-        marks={i*200:f'{i*200}' for i in range(10)}),
-    html.Label('d', htmlFor='d'),
-    dcc.Slider(id='d', min=0, max=3, step=1, value=0,
-        marks={i: f'{i}' for i in range(4)})
+        marks={i*200:f'{i*200}' for i in range(10)})
 ]
-marks = {
-    -1: {'label': '-1.0'},
-    -0.5: {'label': '-0.5'},
-    0: {'label': '0.0'},
-    0.5: {'label': '0.5'},
-    1: {'label': '1.0'},
-}
-for s in ['AR', 'MA']:
-    for i in range(4):
-        v = 0.5 if i == 0 else 0
-        sidebar.append(html.Label(f"{s}({i+1})", htmlFor=f"{s}{i+1}"))
-        sidebar.append(dcc.Slider(id=f"{s}{i+1}", min=-1, max=1, step=0.1, 
-        value=v, marks=marks))
-sidebar.append(
-    html.Button('Gerar novamente', id='regen')
-)
+hyperparam_marks = {i: f'{i}' for i in range(6)}
+for param in ['p','d','q']:
+    value = 0 if param == 'd' else 1
+    sidebar.append(
+        html.Div([
+            html.Label(param),
+            dcc.Slider(id=param, min=0, max=5, step=1, value=value,
+                marks=hyperparam_marks)
+        ])
+    )
+sidebar.append(html.Div(id='ar_container'))
+sidebar.append(html.Div(id='ma_container'))
+sidebar.append(html.Button('Gerar novamente', id='regen'))
 
 
 # ----
@@ -106,6 +100,47 @@ grid = gen_grid([
 # ----
 app.layout = html.Div([navbar, gen_sidebar_layout(sidebar, grid, 3)])
 
+
+
+# ----
+def update_sliders(p, s):
+
+    param_marks = {
+        -1: {'label': '-1.0'},
+        -0.5: {'label': '-0.5'},
+        0: {'label': '0.0'},
+        0.5: {'label': '0.5'},
+        1: {'label': '1.0'},
+    }
+    output = []
+    for i in range(p):
+        value = 0.5 if i == 0 else 0.1
+        output.append(
+            html.Div([
+                html.Label(f"{s}({i+1})", htmlFor=f"{s}{i+1}"),
+                dcc.Slider(id={'type':f'{s}-param-slider', 'index': i}, 
+                    min=-1, max=1, step=0.1, value=value, marks=param_marks)
+            ])
+        )
+    return output
+
+
+@app.callback(
+    Output('ar_container', 'children'),
+    [Input('p', 'value')])
+def update_ar_sliders(p):
+    return update_sliders(p, 'AR')
+
+
+@app.callback(
+    Output('ma_container', 'children'),
+    [Input('q', 'value')])
+def update_ma_sliders(p):
+    return update_sliders(p, 'MA')
+
+
+
+
 # ----
 @app.callback(
     [Output('generated_plot', 'figure'),
@@ -113,18 +148,18 @@ app.layout = html.Div([navbar, gen_sidebar_layout(sidebar, grid, 3)])
      Output('generated_pacf', 'figure')],
     [Input('nsample', 'value'),
      Input('d', 'value'),
-     Input('AR1', 'value'),
-     Input('AR2', 'value'),
-     Input('AR3', 'value'),
-     Input('AR4', 'value'),
-     Input('MA1', 'value'),
-     Input('MA2', 'value'),
-     Input('MA3', 'value'),
-     Input('MA4', 'value'),
+     Input({'type': 'AR-param-slider', 'index': ALL}, 'value'),
+     Input({'type': 'MA-param-slider', 'index': ALL}, 'value'),
      Input('regen', 'n_clicks')])
-def generate_data(nsample, d, ar1, ar2, ar3, ar4, ma1, ma2, ma3, ma4, regen ):
-    ar = -np.array([-1, ar1, ar2, ar3, ar4])
-    ma = np.array([1, ma1, ma2, ma3, ma4])
+def generate_data(nsample, d, ar_params, ma_params, regen ):
+    ar = [1]
+    ma = [1]
+    for item in ar_params:
+        ar.append(item)
+    for item in ma_params:
+        ma.append(item)
+    ar = -np.array(ar)
+    ma = np.array(ma)
     y = sm.tsa.arma_generate_sample(ar, ma, nsample, burnin=1)
     for i in range(d):
         y = np.cumsum(y)
